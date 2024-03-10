4a2a744320d544bfa9954d6d6f31921344329ab8
Rushil Khatri <rushilkhatri1836@gmail.com>
2024-03-10 18:43:40 +0530

    blogs and search block

--- Blog_main/urls.py
+++ Blog_main/urls.py
@@ -19,9 +19,12 @@ from django.contrib import admin
 from django.urls import path,include
 from django.conf.urls.static import static
 from django.conf import settings
+from blogs import views as Blogview
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='home'),
     path('category/', include('blogs.urls')),
+    path('<slug:slug>/',Blogview.blogs, name='blogs' ),
+    path('blogs/search/', Blogview.search, name="search")
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- blogs/views.py
+++ blogs/views.py
@@ -2,6 +2,7 @@ from django.http import HttpResponse
 from django.shortcuts import get_object_or_404, redirect, render
 
 from blogs.models import Blog, Category
+from django.db.models import Q
 
 def posts_by_category(request, category_id):
     posts=Blog.objects.filter(status='published', category=category_id)
@@ -16,3 +17,19 @@ def posts_by_category(request, category_id):
         'categori':categori
     }
     return render(request,'posts_by_category.html', context)
+
+def blogs(request, slug):
+    single_blog= get_object_or_404(Blog, slug=slug, status='published')
+    context={
+        'single_blog':single_blog,
+    }
+    return render(request, 'blog.html', context)
+
+def search(request):
+    keyword=request.GET.get('keyword')
+    blogs=Blog.objects.filter(Q(title__icontains=keyword) | Q(short_description__icontains=keyword), status='published')
+    context={
+        'blogs':blogs,
+        'keyword':keyword,
+    }
+    return render(request, 'search.html', context)
